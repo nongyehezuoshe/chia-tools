26cea68bf395dc324271c66067988cf192497659
Gitea <gitea@fake.local>
2022-08-20 17:14:11 +0800

    update

--- bshholdercut/bshholdercut.py
+++ bshholdercut/bshholdercut.py
@@ -8,7 +8,7 @@ conf={
 	"writer":""
 }
 
-_num=2000
+_num=1000
 _flag=0
 _cut=0
 with f:
@@ -17,6 +17,7 @@ with f:
 		print(row)
 		if _flag%_num==0:
 			conf["writer"]=csv.writer(open('bsh_cut/bshholder_cut_'+str(_cut)+'.csv', 'a'))
+			conf["writer"].writerow(['addr','name','mount'])
 			_cut=_cut+1
 		conf["writer"].writerow(row)
 		_flag=_flag+1
